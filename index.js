c52453521186192d36eb05827cf8fbf8734fb9d1
Regis1272 <21206554+Regis1272@users.noreply.github.com>
2023-07-13 16:05:13 -0700

    Added head(), tail(), at(), and pop() methods

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ console.log(list);
 console.log(list.size());
 
 console.log(list.head());
+
+console.log(list.tail());
+
+/* console.log(list.at(0));
+console.log(list.at(1));
+console.log(list.at(2)); */
+
+
+list.pop();
+// console.log("popped list: " + list);
--- linked-list.js
+++ linked-list.js
@@ -54,6 +54,53 @@ export default class LinkedList {
         return this.listHead;
     }
 
+    tail() {
+        let currNode = this.listHead;
+
+        while (currNode.nextNode !== null) {
+            currNode = currNode.nextNode;
+        }
+
+        return currNode;
+    }
+
+    at(index) {
+        let currNode = this.listHead;
+
+        for (let i = 0; i < index; i++) {
+            currNode = currNode.nextNode;
+        }
+
+        return currNode;
+    }
+
+    pop() {
+        if (this.listHead === null) {
+            console.log("Cannot pop an empty list.")
+        } else {
+            let currNode = this.listHead;
+
+            // Look an extra link ahead for the correct one to pop
+            while (currNode.nextNode.nextNode !== null) {
+                currNode = currNode.nextNode;
+            }
+
+            // log tests
+            // console.log("Current Node:");
+            // console.log(currNode);
+            // console.log("Current Linkage:");
+            // console.log(currNode.nextNode);
+
+            //Sever linkage
+            currNode.nextNode = null;
+
+            // more tests...
+            // console.log("New Linkage:");
+            // console.log(currNode.nextNode);
+        }
+
+    }
+
 
 
 
